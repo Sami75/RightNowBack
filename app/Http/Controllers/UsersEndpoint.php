71be569bf360adb982cdfc50a6a6c2ccbb08d1f5
Sami75 <sami.thairi@hotmail.fr>
2019-01-20 19:33:15 +0100

    login method and correction of store method

--- app/Http/Controllers/UsersEndpoint.php
+++ app/Http/Controllers/UsersEndpoint.php
@@ -15,25 +15,37 @@ class UsersEndpoint extends Controller
 	public function index()
     {
 
-        $data = QueryBuilder::for(User::class)
-            ->paginate()
-        ;
+        $data = User::all();
 
-        return response()->json($data);
+        return response()->json([
+            'data' => $data->toArray(),
+        ]);
     }
 
     public function show(User $user)
     {
+
         return response()->json([
             'data' => $user->toArray(),
         ]);
     }
 
-	public function store(Request $request, User $user)
+	public function store(Request $request)
     {
-        $user = new User();
-        $user->fillFromRequest();
-        $user->saveOrFail();
+        $user = new User([
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'mail' => $request->mail,
+            'password' => $request->password,
+            'numRue' => $request->numRue,
+            'adresse' => $request->adresse,
+            'cdp' => $request->cdp,
+            'ville' => $request->ville,
+            'sexe' => $request->sexe,
+            'telephone' => $request->telephone,
+        ]);
+
+        $user->save();
 
         $user = User::find($user->id);
 
@@ -45,7 +57,7 @@ class UsersEndpoint extends Controller
 
     public function update(Request $request, User $user)
     {
-        $user->fillFromRequest();
+/*        $user->fillFromRequest();
         $user->saveOrFail();
 
         if (!$user->id) {
@@ -57,7 +69,7 @@ class UsersEndpoint extends Controller
 
         return response()->json([
             'data' => User::find($user->id)->toArray(),
-        ]);
+        ]);*/
     }
 
     public function destroy(User $user)
@@ -68,10 +80,26 @@ class UsersEndpoint extends Controller
         return abort(Response::HTTP_NO_CONTENT);
     }
 
-/*    public function login(Request $request, User $user)
-    {
-        $user->fillFromRequest();
-        $user = User::find($user->mail);
+    public function login(Request $request) {
+
+        $user = User::where('mail', $request->mail)->first();
+        return($user);
+        if(isset($user)) {
+            if($user->password == $request->password) {
+                return response()->json([
+                    'data' => $user->toArray(),
+                ]);
+            } else {
+                return response()->json([
+                    'data' => 'Wrong password',
+                ]);
+            }
+        } else {    
+            return response()->json([
+                'data' => 'Wrong password or mail',
+            ]);
+        }
+
     }
-}*/
+}
 
